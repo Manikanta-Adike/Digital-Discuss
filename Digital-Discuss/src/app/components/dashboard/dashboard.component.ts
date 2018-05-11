9f031224eea9f5470e1908c0156ff78b59bc168f
Manikanta-Adike <39184121+Manikanta-Adike@users.noreply.github.com>
2018-05-12 01:58:05 +0530

    Abhishek - Adding DashBoard Code

--- Digital-Discuss/src/app/components/dashboard/dashboard.component.ts
+++ Digital-Discuss/src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+
 })
 export class DashboardComponent implements OnInit {
+
+  public activeTab: String = 'recentQuestion';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public changeTab(selected)  {
+    this.activeTab = selected;
+    const tabs = document.querySelectorAll('side-tab');
+
+    for (let i = 0; i < tabs.length; i++) {
+      tabs[i].className = 'tabs';
+    }
+
+    event.srcElement.className = 'tabs active';
   }
 
 }
