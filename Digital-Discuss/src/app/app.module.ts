448430900dac14f20fa76763d1586adacb9ce55f
Manikanta-Adike <39184121+Manikanta-Adike@users.noreply.github.com>
2018-05-12 01:49:19 +0530

    Adding login popup

--- Digital-Discuss/src/app/app.module.ts
+++ Digital-Discuss/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { RouterModule } from '@angular/router';
@@ -16,6 +17,7 @@ import { routes } from './app.routing';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MaterialModule.forRoot(),
     RouterModule.forRoot(routes)
   ],
