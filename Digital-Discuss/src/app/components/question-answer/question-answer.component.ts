3b365905f715212395d901900a657c4067a3bac9
Manikanta-Adike <39184121+Manikanta-Adike@users.noreply.github.com>
2018-05-12 08:30:34 +0530

    Abhishek - Adding Form Control

--- Digital-Discuss/src/app/components/question-answer/question-answer.component.ts
+++ Digital-Discuss/src/app/components/question-answer/question-answer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-question-answer',
@@ -8,6 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class QuestionAnswerComponent implements OnInit {
 
   constructor() { }
+
+  answerForm = new FormGroup({
+    answer: new FormControl()
+  });
 
   ngOnInit() {
   }
